c9f6335d7dfbb19351b863b743f7e15a802c9d2d
BlazingRockStorm <rocker.gryphon@gmail.com>
2021-11-05 01:07:09 +0900

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :songs, except: [:new, :edit]
     resources :genres, except: [:new, :edit]
   end
+  root to: "api/songs#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
