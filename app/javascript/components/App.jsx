320d296ec1c93c017200aae45ba4e257a5bededa
BlazingRockStorm <rocker.gryphon@gmail.com>
2021-12-26 23:42:03 +0900

    add bootstrap and navbar

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,11 +4,13 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import NavigationBar from './NavigationBar'
 
 function App() {
   return (
     <div>
       <Header />
+      <NavigationBar />
       <Routes>
         <Route path='/' element={<Home/>} />
       </Routes>
