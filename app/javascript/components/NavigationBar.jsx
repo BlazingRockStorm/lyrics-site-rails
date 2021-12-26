d60d9ce9ff50bb3864f71f75f7f460fcf27a2de9
BlazingRockStorm <rocker.gryphon@gmail.com>
2021-12-27 01:19:05 +0900

    add temp style

--- app/javascript/components/NavigationBar.jsx
+++ app/javascript/components/NavigationBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import './navigationbar.css'
 
 function NavigationBar() {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar variant='dark' className='lyric_site_navbar'>
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+        <Nav className='me-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#features'>Features</Nav.Link>
+          <Nav.Link href='#pricing'>Pricing</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
