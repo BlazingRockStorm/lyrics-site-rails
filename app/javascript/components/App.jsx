f6569ce06e6faa64f067e25029bd95aa55cc15ac
BlazingRockStorm <rocker.gryphon@gmail.com>
2021-12-26 20:57:43 +0900

    add header component and footer component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<Home/>} />
       </Routes>
+      <Footer />
     </div>
   )
 }
