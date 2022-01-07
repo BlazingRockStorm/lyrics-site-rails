d158212051a5c4966f74ce7ccb6c41005522ffd1
BlazingRockStorm <rocker.gryphon@gmail.com>
2022-01-07 22:23:19 +0900

    break into smaller component

--- app/javascript/components/Song/Song.jsx
+++ app/javascript/components/Song/Song.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router'
-import { styled } from '@mui/material'
-import { Link } from 'react-router-dom'
-import { Card, Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
+import Detail from './Detail'
 
 function Song() {
   const [song, setSong] = useState([])
@@ -15,26 +14,15 @@ function Song() {
       .catch(data => console.log('error', data))
   }, [])
 
-  const Lyric = styled('div')({
-    whiteSpace: 'pre-wrap'
-  })
-
   return (
     <Container>
       <Row>
         <Col md={9}>
-          <Card>
-            <Card.Header>
-              <Card.Title>{song.name}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                <a href={`${song.spotify_link}`} target='_blank' >スポティファイで聞こう！</a>
-              </Card.Subtitle>
-            </Card.Header>
-            <Card.Body>
-              <Lyric>{song.lyric}</Lyric>
-              <Link to='/'>戻る</Link>
-            </Card.Body>
-          </Card>
+          <Detail
+            name={song.name}
+            spotifyLink={song.spotify_link}
+            lyric={song.lyric}
+          />
         </Col>
       </Row>
     </Container>
