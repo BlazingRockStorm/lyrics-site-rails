af538aa60dafd650156014ec96baff2ef5fd8fb4
BlazingRockStorm <rocker.gryphon@gmail.com>
2022-01-10 11:53:04 +0900

    add redirect after submit and refactor code

--- app/javascript/components/NewSong/NewSong.jsx
+++ app/javascript/components/NewSong/NewSong.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import axios from 'axios'
 import NewSongForm from './NewSongForm'
 
 function NewSong() {
-  const [songs, setSongs] = useState([])
-
   function addNewSong(name, lyric, spotify_link) {
     axios
       .post("/api/songs", { song: { name, lyric, spotify_link } })
-      .then(resp => setSongs([songs, resp.data]))
       .catch(data => console.log('error', data))
   }
 
--- app/javascript/components/NewSong/NewSongForm.jsx
+++ app/javascript/components/NewSong/NewSongForm.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const NewSongForm = ({ onNewSong = (f) => f }) => {
   let _name, _lyric, _spotify_link
+  let navigate = useNavigate()
+
   const submit = (e) => {
     e.preventDefault()
     onNewSong(_name.value, _lyric.value, _spotify_link.value)
@@ -11,6 +13,7 @@ const NewSongForm = ({ onNewSong = (f) => f }) => {
     _lyric.value = ''
     _spotify_link.value = ''
     _name.focus()
+    navigate('/', { replace: true })
   }
 
   return (
@@ -22,7 +25,7 @@ const NewSongForm = ({ onNewSong = (f) => f }) => {
 
       <Form.Group className='mb-3'>
         <Form.Label>Lyric</Form.Label>
-        <Form.Control as='textarea' rows={20} ref={(input) => (_lyric = input)} placeholder='Lyric'/>
+        <Form.Control as='textarea' rows={20} ref={(input) => (_lyric = input)} placeholder='Lyric' />
       </Form.Group>
 
       <Form.Group className='mb-3'>
