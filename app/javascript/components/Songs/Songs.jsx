8a6d46f82346d299c0fa8a0030ca02174d552b0b
Hoang Duc Quan <rocker.gryphon@gmail.com>
2022-01-11 12:10:41 +0900

    Delete button (#17)
    * add delete

--- app/javascript/components/Songs/Songs.jsx
+++ app/javascript/components/Songs/Songs.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Song from './Song'
 import { Link } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
 function Songs() {
   const [songs, setSongs] = useState([])
@@ -12,6 +13,22 @@ function Songs() {
       .catch(data => console.log('error', data))
   }, [])
 
+  function handleSongDelete(id, e) {
+    e.preventDefault()
+    axios.delete(`/api/songs/${id}`)
+      .then(resp => {
+        const targetIndex = songs.findIndex( song => {
+          return song.id === resp.data.id
+        })
+        const newSongs = songs.slice();
+        newSongs.splice(targetIndex, 1);
+        setSongs(newSongs)
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+
   const songslist = songs.map((song) => {
     return (
       <Song
@@ -19,15 +36,16 @@ function Songs() {
         id={song.id}
         name={song.name}
         spotifyLink={song.spotify_link}
+        onDelete={handleSongDelete}
       />
     )
   })
 
   return (
-    <div>
+    <Container fluid>
       {songslist}
       <Link className='btn btn-primary' to='/songs/new'>新歌詞を追加する</Link>
-    </div>
+    </Container>
   )
 }
 
