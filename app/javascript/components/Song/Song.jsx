320a07435f36d9671fb8784b835c183e8c2f5918
BlazingRockStorm <rocker.gryphon@gmail.com>
2022-01-11 16:35:46 +0900

    fix commit

--- app/javascript/components/Song/Song.jsx
+++ app/javascript/components/Song/Song.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router'
+import { useParams } from 'react-router-dom'
 import { Col, Container, Row } from 'react-bootstrap'
 import Detail from './Detail'
 
